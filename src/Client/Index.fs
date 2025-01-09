d77e6b8193bbb0e47469bfe762bbac9d04cfb645
DragosIlies <ilies9dragos@gmail.com>
2025-01-09 19:55:51 +0000

    added dragging feature

--- src/Client/Index.fs
+++ src/Client/Index.fs
@@ -5,7 +5,6 @@ open SAFE
 open Shared
 open System
 
-
 type ButtonState =
     | Normal
     | Hover
@@ -125,54 +124,138 @@ let update msg model =
 
 open Feliz
 
+
+let clamp (value: int) (minVal: int) (maxVal: int) =
+    if value < minVal then minVal
+    elif value > maxVal then maxVal
+    else value
+
 module ViewComponents =
 
     let xpButtonNormal = "xp_btn_norm.png"
     let xpButtonHover = "xp_btn_hover.png"
     let xpButtonClicked = "xp_btn_clicked.png"
 
-    let windowTopBar (window:Window) = Html.div [ 
-        prop.style [
-            style.backgroundImage "linear-gradient(#1f2f86, #3165c4 3%, #3682e5 6%, #4490e6 10%, #3883e5 12%, #2b71e0 15%, #2663da 18%, #235bd6 20%, #2258d5 23%, #2157d6 38%, #245ddb 54%, #2562df 86%, #245fdc 89%, #2158d4 92%, #1d4ec0 95%, #1941a5 98%)"
 
-        ]
-        prop.children [
-            //left
-            Html.div [
-                prop.className "flex items-center gap-1"
-                prop.children [
-                    //Icon
-                    Html.div [
-                        prop.style [
-                            style.marginLeft 5
-                            style.backgroundSize "contain"
-                            style.width 16
-                            style.height 16
-                            style.backgroundImageUrl $"app_icons/mycomputer.png"
+    [<ReactComponent>] //TODO SHOULD MOVE PARENT NOT ONLY TOP BAR, USE ELMISH REACT OON PARENT TO CREATE LOCAL MVU PATTERN
+    let windowTopBar (window:Window) = 
+        let (isDragging, setDragging) = React.useState(false)
+        let (mouseInitialCoord, setInitialMouseCoord) = React.useState((0,0))
+        let (elementCoord, setElementCoord) = React.useState(Some (0,0)) 
+        let (windowPos, setWindowPos) = React.useState((100, 100)) // set position not change relatively
+
+        let windowRef = React.useRef<Browser.Types.HTMLElement option>(None)
+
+        let onMouseHeld (e: Browser.Types.MouseEvent) =
+            setDragging(true)
+            setInitialMouseCoord((e.clientX |> int, e.clientY |> int))
+
+        let onMouseMoving (e: Browser.Types.MouseEvent) =
+            if isDragging then
+                let mouseX, mouseY = mouseInitialCoord
+                let windowX, windowY = windowPos
+                let offsetX = (e.clientX |> int) - mouseX
+                let offsetY = (e.clientY |> int) - mouseY
+
+
+                // Clamp position to viewport boundaries
+
+                setWindowPos((windowX + offsetX, windowY + offsetY))
+                setInitialMouseCoord((e.clientX |> int, e.clientY |> int))
+        
+        let onMouseLeft (_: Browser.Types.MouseEvent) = setDragging(false)
+        
+        //move this div by manipulating its x and y
+        Html.div [ 
+            let button (imgUrl) = 
+
+                //TODO: use react hook? for hover 
+                Html.div [
+                    prop.style [
+                        style.backgroundSize "contain"
+                        style.width 22
+                        style.height 22
+                        style.backgroundImageUrl imgUrl//$"window/Minimise/minimise.png"
+                    ]
+                ]
+
+            //GET current's element X and Y
+            //TODO: Awkward, find out if theres a better way to get current div x and y aka rect left and top
+            // let elementRef = React.useRef<Browser.Types.HTMLElement option>(None)
+            // let elementCoord =
+            //     elementRef.current
+            //     |> Option.map (fun el ->
+            //         let rect = el.getBoundingClientRect()
+            //         Browser.Dom.console.log($"{rect.left}")
+            //         rect.left |> int, rect.top |> int
+            //     )
+            //     |> setElementCoord
+            
+            // //Handle mouse events for dragging and capturing x and y 
+            // prop.onMouseMove ( fun mouseEvent -> 
+            //     setMouseCoord((mouseEvent.clientX |> int,mouseEvent.clientY |> int))
+            //     Browser.Dom.console.log($"X: {mouseEvent.clientX} Y: {mouseEvent.clientY}"))
+            // prop.onMouseDown(fun _ -> setDragging(true))
+            // prop.onMouseUp(fun _ -> setDragging(false))
+            prop.onMouseMove onMouseMoving
+            prop.onMouseDown onMouseHeld
+            prop.onMouseUp onMouseLeft
+
+            
+
+            prop.className "flex place-content-between"
+            prop.style [
+                style.backgroundImage "linear-gradient(#1f2f86, #3165c4 3%, #3682e5 6%, #4490e6 10%, #3883e5 12%, #2b71e0 15%, #2663da 18%, #235bd6 20%, #2258d5 23%, #2157d6 38%, #245ddb 54%, #2562df 86%, #245fdc 89%, #2158d4 92%, #1d4ec0 95%, #1941a5 98%)"
+                let x,y = windowPos
+                //style.transform.translateY y
+                style.transform.translate(x,y)
+            ]
+            prop.children [
+                //left
+                Html.div [
+                    prop.className "flex items-center gap-1"
+                    prop.children [
+                        //Icon
+                        Html.div [
+                            prop.style [
+                                style.marginLeft 5
+                                style.backgroundSize "contain"
+                                style.width 16
+                                style.height 16
+                                style.backgroundImageUrl $"app_icons/mycomputer.png"
+                            ]
+                        ]
+                        //Name
+                        Html.div [
+                            prop.style [
+                                style.fontFamily "Tahoma, sans-serif"
+                                //style.fontWeight 700
+                                style.fontSize 12
+                                style.color "white"
+                                //style.custom("textShadow", "rgb(0, 0, 0) 1px 1px")
+                            ]
+                            prop.text window.Shortcut.Name
                         ]
                     ]
-                    //Name
-                    Html.div [
-                        prop.style [
-                            style.fontFamily "Tahoma, sans-serif"
-                            //style.fontWeight 700
-                            style.fontSize 12
-                            style.color "white"
-                            //style.custom("textShadow", "rgb(0, 0, 0) 1px 1px")
-                        ]
-                        prop.text window.Shortcut.Name
+                ]
+                //right
+                Html.div [
+                    prop.className "flex"
+                    prop.children [
+                        button "window/Minimise/minimise.png"
+                        button "window/Resize/maximise.png"
+                        button "window/Close/close.png"
                     ]
                 ]
             ]
-            //right
         ]
-    ]
 
     let windowDisplay (window:Window) = 
         Html.div [
             prop.style [
                 style.backgroundColor "rgb(8, 49, 217)"
                 style.borderRadius 5
+                style.zIndex 1
             ]
 
             prop.children [
@@ -181,11 +264,21 @@ module ViewComponents =
             ]
         ]
 
-    let windowsDisplayed (windows: Window list) (dispatch:Msg -> Unit) =
+    let windowsDisplayed (windows: Window list) (dispatch: Msg -> Unit) =
         Html.div [
-            for window in windows do
-                if window.Active then
-                    windowDisplay window
+            prop.style [
+                style.position.absolute
+                style.top 0 // Align at the top of the container
+                style.left 0 // Align at the left of the container
+                style.zIndex 10 
+                style.width 1000 //Temporary
+            ]
+            prop.children [
+                for window in windows do
+                    if window.Active then
+                        //windowDisplay window
+                        windowTopBar (window:Window)
+            ]
         ]
 
     let windowsShortcut (windowsShortcut: WindowsShortcut) (dispatch:Msg -> Unit) = 
@@ -232,6 +325,8 @@ module ViewComponents =
 
     let taskbar model dispatch = 
 
+
+        //TODO Use React useState to isolate and declutter Update
         let buttonBackground =
             match model.StartButtonState with
                 | Normal -> xpButtonNormal
@@ -334,16 +429,23 @@ module ViewComponents =
 
 let view (model:Model) (dispatch:Msg -> Unit) =
     Html.section [
-        prop.className "h-screen w-screen grid gap-4 grid-rows-[1fr_auto] grid_cols-[auto_1fr]"
-        prop.style [
-            style.backgroundSize "cover"
-            style.backgroundImageUrl "bg.jpg"
-            //style.display.grid; //or use directly in class name
-        ]
-        prop.onClick(fun _ -> dispatch DeselectWindowsShortcuts)
         prop.children [
-            ViewComponents.listApps model dispatch
-            ViewComponents.taskbar model dispatch
+            Html.div [
+                prop.className "h-screen w-screen grid gap-4 grid-rows-[1fr_auto] grid_cols-[auto_1fr]"
+                prop.style [
+                    style.backgroundSize "cover"
+                    style.backgroundImageUrl "bg.jpg"
+                    //style.display.grid; //or use directly in class name
+                ]
+                prop.onClick(fun _ -> dispatch DeselectWindowsShortcuts)
+                prop.children [
+                    ViewComponents.listApps model dispatch
+                    ViewComponents.taskbar model dispatch
+                    //ViewComponents.windowsDisplayed model.Windows dispatch
+                ]
+            ]
+
             ViewComponents.windowsDisplayed model.Windows dispatch
         ]
+
     ]
